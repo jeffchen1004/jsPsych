0c04810e4d86771872f7da4e56285f30ff5c9aab
jeffchen1004 <gschen@ucdavis.edu>
2024-05-03 18:00:54 -0700

    record-midi plugin

--- plugins/jspsych-record-midi.js
+++ plugins/jspsych-record-midi.js
@@ -1,195 +1,249 @@
 // Plugin
 // MIDI API and trial details
 
-jsPsych.plugins['record-midi'] = (function() {
+// The pipeline should be
+// 1. Start MIDI Access
+// 2. Start Timer at experiment start
+// 3. Start Metronome
+// 4. Record and collect MIDI and timing data
 
+jsPsych.plugins['record-midi'] = (function() {
+    // Empty Javascript object to store info in plugins
     var plugin = {};
 
+    var keyboardListener;
+    var display_element;
+    var startTime; // Define this globally
+    var recordedMIDIData = [];
+
+    // Plugin info; the parameters
     plugin.info = {
-        name: 'record-midi',
-        description: 'Web MIDI API Integration',
-        parameters: {
-            /* stimulus: {
-                type: jsPsych.plugins.parameterType.AUDIO,
-                default: undefined
-            }, */
-            click_to_start: {
-                type: jsPsych.plugins.parameterType.BOOL,
-                default: true
-            },
+        name: 'record-midi', // should match the name of the plugin file
+        description: 'Web MIDI API Integration', // not important
+        parameters: {     
             prompt: {
-                type: jsPsych.plugins.parameterType.HTML_STRING,
-                default: ''
+                type: jsPsych.plugins.parameterType.HTML_STRING, // The type of the parameter
+                default: 'Get Ready! Press Y to start' // Default value of the parameter
             },
             trial_duration: {
                 type: jsPsych.plugins.parameterType.INT,
-                default: 3000
+                default: 30000 //ms
             },
             metronome_bpm: {
                 type: jsPsych.plugins.parameterType.INT,
                 default: 85
             },
-            metronome_delay: {
-                type: jsPsych.plugins.parameterType.INT,
-                default: 3000
-            },
             metronome_beats: {
                 type: jsPsych.plugins.parameterType.INT,
-                default: null
+                default: null // null means play indefinitely
+            },
+            metronome_condition: {
+                type: jsPsych.plugins.parameterType.STRING,
+                default: 'throughout',
+                options: ['none', 'beginning', 'throughout']
             },
         }
     };
 
-    // Function to start the metronome
-    plugin.startMetronome = function(bpm, numberOfBeats, startDelay) {
+    // Start MIDI Access
+    function startMIDIAccess() {
+        if (navigator.requestMIDIAccess) {
+            navigator.requestMIDIAccess({ sysex: false }).then(onMIDISuccess, onMIDIFailure);
+        } else {
+            console.log("Web MIDI API not supported in this browser, change to Chrome!");
+        }
+    }
+
+    // Now it actually checks MIDI input instead of just browser compatibility (I think)
+    function onMIDISuccess(midiAccess) {
+        console.log("MIDI Access successful! Reached the onMIDISuccess function");
+        var inputs = midiAccess.inputs;
+        if (inputs.size === 0) {
+            console.log("No MIDI inputs available");
+        } else {
+            var inputValues = inputs.values();
+            for (var input = inputValues.next(); input && !input.done; input = inputValues.next()) {
+                input.value.onmidimessage = onMIDIMessage;
+            }
+        }
+    }
+
+
+    function onMIDIFailure() {
+        console.log("Cannot access the MIDI device!");
+    }
+
+
+    // Important to have this function to process and collect incoming MIDI data
+    function onMIDIMessage(message) {
+        console.log("MIDI message received:", message);
+        var data = message.data;
+        var elapsedTime = performance.now() - startTime; // performance.now is the time right now, so minus the startTime to get the elapsed time
+
+        recordedMIDIData.push({
+            midiMessage: data,
+            timestamp: elapsedTime
+        });
+
+        updateMIDIData({mididata: data, time: elapsedTime});
+    }
+
+    // Updates the data collected in the onMIDIMessage function
+    function updateMIDIData(data) {
+        var midiDataDiv = document.getElementById('midi-data');
+        if (midiDataDiv) {
+            midiDataDiv.innerHTML = "MIDI Data: " + JSON.stringify(data.mididata) + ", Elapsed Time: " + data.time + "ms";
+        }
+    }
+
+/*
+    // Updates the data collected in the onMIDIMessage function
+    function updateMIDIData(data, elapsedTime) {
+        var midiDataDiv = document.getElementById('midi-data');
+        if (midiDataDiv) {
+            midiDataDiv.innerHTML = "MIDI Data: " + JSON.stringify(data) + ", Elapsed Time: " + elapsedTime + "ms";
+            // if we want to display the midi-data, we need "<div id="midi-data"></div>" in the HTML file
+            //midiDataDiv.innerHTML = "MIDI Data: " + JSON.stringify(data) + 
+            //                        " at " + elapsedTime + "ms";
+        }
+    }; 
+*/ 
+    
+    
+
+    // Start timer function
+    function startTimer() {
+        startTime = performance.now();
+    }
+
+
+    // Start metronome function here? I think so
+    function startMetronome(bpm, numberOfBeats) {
+        console.log("startMetronome reached")
+
+        // Create an AudioContext object
         var context = new (window.AudioContext || window.webkitAudioContext)();
-
+    
         // Calculate the delay time between each beat
-        var beatInterval = 60 / bpm * 1000;
-
+        var beatInterval = 60 * 1000 / bpm;
+    
         // Function to play a beep sound
         var playBeep = function() {
             var osc = context.createOscillator();
             osc.type = 'sine'; // Experiment with different waveforms: sine, square, sawtooth, triangle
-
+    
             osc.connect(context.destination);
             osc.start();
             osc.stop(context.currentTime + 0.1); // The beep lasts for 0.1 seconds
         };
+    
+        // Start the metronome
+        var beatCount = 0;
 
-        // Start the metronome after the specified start delay
+        var metronomeInterval = setInterval(function() {
+            playBeep();
+            beatCount++;
+            if (numberOfBeats && beatCount >= numberOfBeats) {
+                clearInterval(metronomeInterval);
+                // DO NOT endTrial HERE
+            }
+        }, beatInterval);
+
+        return metronomeInterval;
+    }
+
+
+    // After the participant presses Y, the trial starts
+    function startTrial(display_element, trial) {
+        console.log("Trial Data", trial)
+        console.log("startingTrial reached should have metronome and midi input")
+
+        // Start MIDI Access
+        startMIDIAccess();
+
+        // Start the timer
+        // This is the start time of the experiment
+        startTimer(); 
+
+        var metronomeInterval;
+
+        // Start the metronome based on the metronome_condition parameter
+        switch (trial.metronome_condition) {
+            case 'throughout':
+                console.log("'throughout metronome' condition reached");
+                metronomeInterval = startMetronome(trial.metronome_bpm, trial.metronome_beats);
+                break;
+            case 'beginning':
+                console.log("'beginning metronome' condition reached");
+                // Start the metronome, but stop it after a certain number of beats
+                metronomeInterval = startMetronome(trial.metronome_bpm, 10); // 10 beats
+                break;
+            case 'none':
+                console.log("'no metronome' condition reached");
+                // Don't start the metronome
+                break;
+        }
+
         setTimeout(function() {
-            plugin.metronomeInterval = setInterval(function() {
-                playBeep();
+            endTrial(display_element, recordedMIDIData, trial); // endTrial after the trial duration
+            if (metronomeInterval) {
+                clearInterval(metronomeInterval); // Stop the metronome if it's still playing
+            }
+        }, trial.trial_duration);
+    };
+    
 
-                if (numberOfBeats) {
-                    numberOfBeats--;
-                    if (numberOfBeats <= 0) {
-                        clearInterval(plugin.metronomeInterval);
-                    }
+    // Function that jsPsych calls when it's time to run the trial
+    // 2 arguments rn, display_element and trial
+    plugin.trial = function(de, trial) {
+        display_element = de; // Assign the passed display element to the outer variable
+        // Sets HTML content of display_element to value of trial.prompt
+        // HTML element to display the prompt = object containing the parameters for the current trial
+        display_element.innerHTML = trial.prompt;
+
+        // Wait for participants to press y to start
+        jsPsych.pluginAPI.getKeyboardResponse({
+            callback_function: function(info) {
+                console.log("Key pressed:", info.key);
+                // Start the trial when 'Y' or 'y' is pressed
+                if (info.key === 'y' || info.key === 'Y') {
+                    console.log("Starting trial")
+                    startTrial(display_element, trial); // have to define this later as a function 
                 }
-            }, beatInterval);
-        }, startDelay);
+            },
+            valid_responses: ['y', 'Y'], // keycodes for 'Y' and 'y'
+            rt_method: 'performance', // reation time measured using performace.now()
+            persist: false, // whether keyboard should persist after being pressed, False = stop after key press
+            allow_held_key: false // don't want this to be true, self explainatory
+        });
     };
 
 
-    plugin.trial = function (display_element, trial) {
-        // Web MIDI API Integration
-        let startTime; // This will hold the start time of recording
-        let midiData = []; // For MIDI Data
-        let metronomeInterval; // Storing metronome interval function
+    // Function to end the trial
+    function endTrial(trial) {
+    
+        // clear keyboard response
+        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+    
+        // save data
+        var trial_data = {
+            midi_data: recordedMIDIData, // Add the MIDI data
+            metronome_condition: trial.metronome_condition, // Add the metronome condition
+        };
+    
+        display_element.innerHTML = '';
+    
+        // next trial
+        jsPsych.finishTrial(trial_data);
 
-        function handleMIDIMessage(message) {
-            let currentTime = performance.now();
-            if (!startTime) { // Set startTime on the first MIDI message
-                startTime = currentTime;
-            }
-            let elapsedTime = currentTime - startTime; // Time since start of trial
-
-            midiData.push({
-                data: message.data,
-                timestamp: elapsedTime
-            });
-            // Start the metronome based on trial parameters
-            if (trial.useMetronome) {
-                plugin.startMetronome(trial.metronome_bpm, trial.metronome_beats, trial.metronome_delay);
-            }
-
-            updateMidiData(message.data, elapsedTime);
-        }
-
-        // Function to update the display of MIDI data
-        function updateMidiData(data, elapsedTime) {
-            var midiDataDiv = document.getElementById('midi-data');
-            if (midiDataDiv) {
-                midiDataDiv.innerHTML = "MIDI Data: " + JSON.stringify(data) + 
-                                        " at " + elapsedTime + "ms";
-            }
-        }
-
-        // Function to start the metronome
-        function startMetronome(bpm, beats) {
-            let beatLength = 60 * 1000 / bpm;
-            let beatCount = 0;
-            metronomeInterval = setInterval(function() {
-                console.log("Metronome tick");
-                beatCount++;
-                if (beats && beatCount >= beats) {
-                    clearInterval(metronomeInterval);
-                }
-            }, beatLength);
-        }
-
-        // Function to request MIDI access
-        function requestMIDIAccess() {
-            if (navigator.requestMIDIAccess) {
-                navigator.requestMIDIAccess({ sysex: false }).then(onMIDISuccess, onMIDIFailure);
-            } else {
-                console.warn("Web MIDI API not supported in this browser");
-            }
-        }
-
-        // Function to handle successful MIDI connection
-        function onMIDISuccess(midiAccess) {
-            console.log("MIDI Access Success");
-            var inputs = midiAccess.inputs.values();
-            for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-                input.value.onmidimessage = handleMIDIMessage;
-            }
-        }
-
-        // Function to handle failed MIDI connection
-        function onMIDIFailure() {
-            console.warn("MIDI Access Failed");
-        }
-
-        // Display the prompt if it exists
-        if (trial.prompt) {
-            display_element.innerHTML = trial.prompt;
-        }
-
-        // Function to start the trial
-        function startTrial() {
-            startTime = performance.now(); // Set the start time for the trial
-
-            // Start the metronome after the specified delay
-            setTimeout(function() {
-                startMetronome(trial.metronome_bpm, trial.metronome_beats);
-            }, trial.metronome_delay);
-
-            // Request MIDI Access
-            requestMIDIAccess();
-
-            // End the trial after trial_duration
-            setTimeout(function() {
-                endTrial();
-            }, trial.trial_duration);
-        }
-
-        // Function to end the trial
-        function endTrial() {
-            clearInterval(metronomeInterval); // Stop the metronome
-            // Gather data and finish the trial
-            jsPsych.finishTrial({
-                midiData: midiData
-            });
-        }
-
-        // Handle the click_to_start logic
-        if (trial.click_to_start) {
-            var startButton = document.createElement('button');
-            startButton.id = 'start_button';
-            startButton.innerHTML = 'Start';
-            display_element.appendChild(startButton);
-
-            startButton.addEventListener('click', function() {
-                startButton.style.display = 'none';
-                startTrial();
-            });
-        } else {
-            startTrial();
-        }
-    };
+        // Save data (did not work, messed the metronome up)
+        // jsPsych.data.get().localSave('data.csv', 'csv');
+    }
 
     return plugin;
 })();
+
+        
+
+
+    
